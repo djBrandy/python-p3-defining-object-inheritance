3471b86b264d3c94c3b8cc3e28604ef344950e29
brandondando <dandobrandon0@gmail.com>
2024-05-29 13:01:18 +0300

    solved

--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,17 @@
 class Vehicle:
+    def __init__(self, wheel_size, wheel_number):
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
     pass
+
+    def go (self):
+        return "vrrrrrrrooom!"
+    
+
+    def fill_up_tank (self):
+        return "filling up!"
+
+
+
+
+wheel_size = Vehicle(48, 4)
